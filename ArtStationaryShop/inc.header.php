4c442d177b70d0067b91b82635fa4584acd4d9d4
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2025-03-03 14:22:30 +0500

    logout created

--- ArtStationaryShop/inc.header.php
+++ ArtStationaryShop/inc.header.php
@@ -261,7 +261,7 @@ if(!$_SESSION['AdminLogin']) {
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="#">
+              <a class="dropdown-item d-flex align-items-center" href="logout.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
---
+++ ArtStationaryShop/logout.php
@@ -0,0 +1,7 @@
+<?php
+include('inc.connection.php');
+session_unset(); // Unset all session variables
+session_destroy(); // Destroy the session
+header("Location: login.php"); // Redirect to login page
+exit();
+?>
